f7ed68dff52c5850d16a797837930f2fe41a01ed
baohaiya <1765094917@qq.com>
2018-11-05 09:24:37 +0800

    11

---
+++ demo/pages/index/index.js
@@ -0,0 +1,56 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/top_1.jpg',
+      '/images/top_2.jpg',
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000, 
+    booksList:[],
+    selectedBooksList:[],
+    nameLike:'',
+    selectedBooksIds:"",
+  },
+  
+  onLoad: function () {
+    this.queryBooks();
+   
+  },
+  nameInput:function(e){
+    this.setData({
+      nameLike: e.detail.value
+    })
+  },
+  queryBooks:function(e){
+    var that = this;  
+    wx.request({
+      url: 'https://www.baohaiya.top/library/books/queryBooks',
+      data: {
+        nameLike: that.data.nameLike
+      },
+      header: {
+        "content-type": "applciation/json" // 默认值
+      },
+      method: "get",
+      success: function (res) {
+        that.setData({
+          booksList: res.data,
+        })
+      },
+      fail:function(){
+        wx.showModal({
+          title: '查询失败',
+          content: '查询书籍失败，请重试。',
+        })
+      }
+    })
+  }
+  
+}) 
+
---
+++ demo/pages/my/my.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/top_1.jpg',
+      '/images/top_2.jpg',
+      '/images/top.jpg',
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    booksList: [],
+    name: '',
+  },
+
+  onLoad: function () {
+   APP
+
+  },
+  
+})
