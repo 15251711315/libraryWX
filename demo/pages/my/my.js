e4c18ac486b7bc7128624e97a8c0e322aac4a78d
baohaiya <1765094917@qq.com>
2018-12-21 16:58:04 +0800

    1

--- demo/pages/my/my.js
+++ demo/pages/my/my.js
@@ -3,16 +3,60 @@
 const app = getApp()
 
 Page({
-  data: {
-    avatarUrl:'',
-    nickName:'' 
-  },
-
   onLoad: function () {
     this.setData({
       avatarUrl: getApp().globalData.userInfo.avatarUrl,
       nickName:getApp().globalData.userInfo.nickName
+    }),
+      this.queryReadBooks()
+  },
+  data: {
+    avatarUrl: '',
+    nickName: '' ,
+    list01: [],
+    list02: [],
+   
+    // 展开折叠
+    selectedFlag: [false, false, false, false],
+
+  },
+  // 展开折叠选择  
+  changeToggle: function (e) {
+    var index = e.currentTarget.dataset.index;
+    if (this.data.selectedFlag[index]) {
+      this.data.selectedFlag[index] = false;
+    } else {
+      this.data.selectedFlag[index] = true;
+    }
+
+    this.setData({
+      selectedFlag: this.data.selectedFlag
     })
   },
-  
+
+  queryReadBooks: function(e){
+    var that = this;
+    wx.request({
+      url: app.globalData.url + "/books/queryReadBooks",
+      data: {
+        userId: app.globalData.userInfo.id
+      },
+      header: {
+        "content-type": "applciation/json" // 默认值
+      },
+      method: "get",
+      success: function (res) {
+        that.setData({
+          list01: res.data.reading,
+          list02: res.data.returned
+        })
+      },
+      fail: function () {
+        wx.showModal({
+          title: '查询失败',
+          content: '查询书籍失败，请重试。',
+        })
+      }
+    })
+  }
 })
